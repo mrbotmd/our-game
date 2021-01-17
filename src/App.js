5485f1ab02f14b30be4876328685595ff67b74f0
mrbotmd <mr.bot.md@gmail.com>
2021-01-17 22:48:26 +0600

    added react-router-dom and basic paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Route } from "react-router-dom";
+import Game from "./component/Game/Game";
+import Header from "./component/Header/Header";
+
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <Route exact path="/">
+        <Header />
+      </Route>
+      <Route exact path="/game" component={Game} />
+    </div>
+  );
 }
 
 export default App;
