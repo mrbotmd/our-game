a11c23225a276ff4c57d04cd7a256e359e3fb7dc
mrbotmd <mr.bot.md@gmail.com>
2021-02-22 23:13:00 +0600

    added server paths

---
+++ src/paths.js
@@ -0,0 +1,15 @@
+const BASE_URL = "http://5.63.112.35:5010/";
+const GAME_PACKS = "game-packs";
+const REGISTER = "register";
+const USER_AUTH = "auth";
+const USER_SESSIONS = "user-sessions";
+const GAME_SESSIONS = "game-sessions";
+
+export {
+  BASE_URL,
+  GAME_PACKS,
+  REGISTER,
+  USER_AUTH,
+  USER_SESSIONS,
+  GAME_SESSIONS,
+};
