8f7a41caccba2c5a5b7a9e0be277a4cf751ba151
mrbotmd <mr.bot.md@gmail.com>
2021-02-23 17:50:35 +0600

    Refactored obtaining access token

--- src/App.js
+++ src/App.js
@@ -3,39 +3,45 @@ import { Route } from "react-router-dom";
 import Game from "./component/Game/Game";
 import GamePackForm from "./component/GamePack/GamePackForm";
 import Header from "./component/Header/Header";
-import axios from "axios";
+// import axios from "axios";
+// import { BASE_URL, USER_SESSIONS } from "./paths";
 import Register from "./Pages/Register/Register";
-import { BASE_URL, USER_SESSIONS } from "./paths";
+import { startUserSession } from "./axiosClient";
+import { IsLoggedInProvider } from "./context/IsLoggedInContext";
 
 function App() {
   const [accessToken, setAccessToken] = useState(
     window.localStorage.getItem("accessToken") || ""
   );
+  console.log("🚀 ~ file: App.js ~ line 13 ~ App ~ accessToken", accessToken);
 
   useEffect(() => {
-    axios({
-      method: "POST",
-      url: BASE_URL + USER_SESSIONS,
-    })
-      .then((res) => {
+    const getAccessToken = async () => {
+      try {
+        const access_token = await startUserSession();
         if (accessToken === "") {
-          window.localStorage.setItem("accessToken", res.data.access_token);
-          setAccessToken(res.data.access_token);
+          window.localStorage.setItem("accessToken", access_token);
+          setAccessToken(access_token);
         }
-      })
-      .catch((err) => console.log(err));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getAccessToken();
   }, [accessToken]);
 
   return (
-    <div className="App">
-      <button onClick={() => window.localStorage.removeItem("accessToken")}>
-        clear token
-      </button>
-      <Route path="/" component={Header} exact />
-      <Route path="/game" component={Game} exact />
-      <Route path="/create-new-gamepack" component={GamePackForm} exact />
-      <Route path="/register" component={Register} exact />
-    </div>
+    <IsLoggedInProvider>
+      <div className="App">
+        <button onClick={() => window.localStorage.removeItem("accessToken")}>
+          clear token
+        </button>
+        <Route path="/" component={Header} exact />
+        <Route path="/game" component={Game} exact />
+        <Route path="/create-new-gamepack" component={GamePackForm} exact />
+        <Route path="/register" component={Register} exact />
+      </div>
+    </IsLoggedInProvider>
   );
 }
 
