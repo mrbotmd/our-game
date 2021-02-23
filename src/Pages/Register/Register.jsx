c32ff7ef61c78002d9a9f0f4aecc8fe1ea679319
mrbotmd <mr.bot.md@gmail.com>
2021-02-23 17:51:25 +0600

    refactored register func

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { Formik, Field, Form } from "formik";
-import axios from "axios";
-import { BASE_URL, REGISTER } from "../../paths";
 import { useHistory } from "react-router-dom";
+import { registerUser } from "../../axiosClient";
 
 export default function Register() {
   const [accessToken] = useState(window.localStorage.getItem("accessToken"));
+  const [username, setUsername] = useState("");
   const history = useHistory();
   console.log(
     "🚀 ~ file: Register.jsx ~ line 7 ~ Register ~ accessToken",
@@ -20,28 +20,7 @@ export default function Register() {
           password: "",
         }}
         onSubmit={async (values) => {
-          console.log(values);
-          const valuesToStr = JSON.stringify(values);
-          console.log(typeof valuesToStr);
-          console.log(
-            "🚀 ~ file: Register.jsx ~ line 23 ~ onSubmit={ ~ valuesToStr",
-            valuesToStr
-          );
-          axios({
-            url: BASE_URL + REGISTER,
-            method: "POST",
-            data: valuesToStr,
-            headers: {
-              access_token: accessToken,
-            },
-          })
-            .then((res) => console.log(res))
-            .then(async () =>
-              setTimeout(() => {
-                history.push("/");
-              }, 2000)
-            )
-            .catch((err) => console.log(err));
+          await registerUser(accessToken, values);
         }}
       >
         <Form>
