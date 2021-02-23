8d5ec25cea05469485128d24e7bf3cb9d5ad3abe
mrbotmd <mr.bot.md@gmail.com>
2021-02-23 17:49:26 +0600

    Changed paths

--- src/paths.js
+++ src/paths.js
@@ -1,6 +1,6 @@
 const BASE_URL = "http://5.63.112.35:5010/";
 const GAME_PACKS = "game-packs";
-const REGISTER = "register";
+const USER_REGISTER = "register";
 const USER_AUTH = "auth";
 const USER_SESSIONS = "user-sessions";
 const GAME_SESSIONS = "game-sessions";
@@ -8,7 +8,7 @@ const GAME_SESSIONS = "game-sessions";
 export {
   BASE_URL,
   GAME_PACKS,
-  REGISTER,
+  USER_REGISTER,
   USER_AUTH,
   USER_SESSIONS,
   GAME_SESSIONS,
